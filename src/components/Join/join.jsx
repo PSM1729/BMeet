deccdf170923b9551c8e5ada38e9ca20551f02ce
PSM1729 <sabitamishra1729@gmail.com>
2021-01-25 20:50:43 +0530

    Added join us form

--- src/components/Join/join.jsx
+++ src/components/Join/join.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import bg1 from '../../images/2.png';
 import './join.css';
-import { Container } from 'reactstrap';
 
 const Join = () => {
     return (
-        <div style={{backgroundColor:'#D4EDEB' ,height:'700px',textAlign:'center'}}>
-            <div className="meet">
-                <img src={bg1} alt="VIDEO MEET" />
+        <div style={{backgroundColor:'#D4EDEB' ,height:'700px',textAlign:'center',display:'flex'}}>
+            <img src={bg1} alt="VIDEO MEET" />
+            <div className="box">
+                <h2>JOIN A MEET</h2>
+                <input></input><br/>
+                <button>JOIN</button>
+                <br /><br/>
+                <h2>CREATE A MEET</h2>
+                <button>CREATE</button>
             </div>
         </div>
     )
