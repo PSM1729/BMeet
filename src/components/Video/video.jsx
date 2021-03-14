f92b0c2c2ca9e892a47abd1efc8bd48d6660af89
Prachhi Sabita Mishra <62738461+PSM1729@users.noreply.github.com>
2021-03-14 23:22:54 +0530

    Bugs removed

--- src/components/Video/video.jsx
+++ src/components/Video/video.jsx
@@ -20,7 +20,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://bmeet.com' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? 'https://bomeet-b49de.web.app/' : "http://localhost:4001"
 
 var connections = {}
 const peerConnectionConfig = {
